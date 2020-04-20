a440a147650a35a96bdc4507662d2ce0b1c78c92
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-04-20 00:49:32 -0400

    swiper orientado a objeros

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:cine_app/src/widgets/SwiperContainerWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -6,9 +8,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea( 
-        child: Text("HOLA MARIIIIIIII") ),
+      appBar: AppBar(
+        title:  Text("AppBar"),
+        backgroundColor: Colors.indigo,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search), 
+            onPressed: (){})
+        ],
+      ),
+      body: Container(
+        child: _swiper(),
+      )
       );
+  }
+
+  Widget _swiper() {
+    return SwiperContainerWidget(peliculas: [1,2,3]);
   }
 
 }
---
+++ lib/src/widgets/SwiperContainerWidget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class SwiperContainerWidget extends StatelessWidget {
+
+  final List<dynamic> peliculas ;
+  SwiperContainerWidget({@required this.peliculas});
+
+
+  @override
+  Widget build(BuildContext context) {
+  final _screenSize = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.only(top: 5),
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: _screenSize.width * 0.7,
+        itemHeight: _screenSize.height * 0.5,
+            itemBuilder: (BuildContext context, int index) {
+              return Image.network('https://vignette.wikia.nocookie.net/cartoonnetwork/images/d/dc/221975_10150172117895680_1273681_n.jpg/revision/latest/scale-to-width-down/340?cb=20130921015205&path-prefix=es',
+                fit: BoxFit.cover,
+              );
+            },
+            itemCount: 3,
+            //pagination: SwiperPagination(),
+            //control: SwiperControl(),
+          ),
+    );
+  }
+}
