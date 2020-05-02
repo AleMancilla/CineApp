de2be03ee7c14b61e778e5986b09c7f8b1299e9e
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-05-01 23:36:48 -0400

    sliverAppBar dentro de un CustomScrollView

--- lib/src/pages/PeliculaDetalle.dart
+++ lib/src/pages/PeliculaDetalle.dart
@@ -8,9 +8,31 @@ class PeliculaDetalle extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
+    
     return Container(
       child: Scaffold(
-        body: Center(child: Text(pelicula.title)),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            _crearAppBar(pelicula),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearAppBar(Pelicula pelicula) {
+    return SliverAppBar(
+      elevation: 2.0,
+      backgroundColor: Colors.indigoAccent,
+      expandedHeight: 200.0,
+      floating: true,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        title: Text(
+          pelicula.title,
+          style: TextStyle(color: Colors.white, fontSize: 16.0, ),
+        ),
       ),
     );
   }
