e03f7bacb733d78977223de1b745bf49d6f03947
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-05-03 21:56:02 -0400

    Search delegate

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -19,7 +19,11 @@ class HomePage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search), 
-            onPressed: (){})
+            onPressed: (){
+              showSearch(
+                context: null, 
+                delegate: null);
+            })
         ],
       ),
       body: Container(
---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate{
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: las acciones de nuestro app bar
+    return null ;
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: icono a la izquierda del appbar
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    return null;
+  }
+
+}
